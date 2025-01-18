b29038b699b57aa3b8dd3cbffe0a517e45e58a43
Svetlana Khlystova <khlystova_sv@bk.ru>
2025-01-18 13:15:05 +0300

    Modify by constructors

--- src/main/java/ru/netology/Radio/objects/Radio.java
+++ src/main/java/ru/netology/Radio/objects/Radio.java
@@ -1,11 +1,31 @@
 package ru.netology.Radio.objects;
 
 public class Radio {
-    private int currentStation;
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int currentStation = minStation;
     private int currentVolume;
+
+    public Radio() {
+        this.minStation = minStation;
+        this.maxStation = maxStation;
+        this.currentStation = currentStation;
+    }
+
+    public Radio(int stations) {
+        maxStation = minStation + stations - 1;
+    }
 
     public int getCurrentStation() {
         return currentStation;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public int getMinStation() {
+        return minStation;
     }
 
     public int getCurrentVolume() {
@@ -13,7 +33,7 @@ public class Radio {
     }
 
     public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation > 9) {
+        if (newCurrentStation > maxStation) {
             return;
         }
         if (newCurrentStation < 0) {
@@ -23,7 +43,7 @@ public class Radio {
     }
 
     public void setNextStation() {
-        if (currentStation < 9) {
+        if (currentStation < maxStation) {
             currentStation++;
         } else {
             currentStation = 0;
@@ -34,7 +54,7 @@ public class Radio {
         if (currentStation > 0) {
             currentStation--;
         } else {
-            currentStation = 9;
+            currentStation = maxStation;
         }
     }
 
